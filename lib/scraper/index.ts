c74017b242f2affc862db500e988417d7add0782
samaksh mittal <sam.mittal44@gmail.com>
2024-08-07 19:50:22 +0530

    feat: extract currency image, price, high, low, descript by scraping amazon product page

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 import * as cheerio from "cheerio";
-import { extractPrice } from "../utils";
+
+import { extractCurrency, extractDescription, extractPrice } from "../utils";
 
 export async function scrapeAmazonProduct({ url }: { url: string }) {
   if (!url) return;
@@ -24,8 +25,60 @@ export async function scrapeAmazonProduct({ url }: { url: string }) {
     const $ = cheerio.load(response.data);
 
     const title = $("#productTitle").text().trim();
-    const price = extractPrice({ elements });
-    console.log(title);
+    const currentPrice = extractPrice({
+      elements: [
+        $(".priceToPay span.a-price-whole"),
+        $(".a.size.base.a-color-price"),
+        $(".a-button-selected .a-color-base"),
+      ],
+    });
+
+    const originalPrice = extractPrice({
+      elements: [
+        $("#priceblock_ourprice"),
+        $(".a-price.a-text-price span.a-offscreen"),
+        $("#listPrice"),
+        $("#priceblock_dealprice"),
+        $(".a-size-base.a-color-price"),
+      ],
+    });
+
+    const outOfStock =
+      $("#availability span").text().trim().toLowerCase() ===
+      "currently unavailable";
+
+    const images =
+      $("#imgBlkFront").attr("data-a-dynamic-image") ||
+      $("#landingImage").attr("data-a-dynamic-image") ||
+      "{}";
+
+    const imageUrls = Object.keys(JSON.parse(images));
+
+    const currency = extractCurrency({ element: $(".a-price-symbol") });
+    const discountRate = $(".savingsPercentage").text().replace(/[-%]/g, "");
+
+    const description = extractDescription({ cheerio$: $ });
+
+    const data = {
+      url,
+      currency: currency || "$",
+      image: imageUrls[0],
+      title,
+      currentPrice: Number(currentPrice) || Number(originalPrice),
+      originalPrice: Number(originalPrice) || Number(currentPrice),
+      priceHistory: [],
+      discountRate: Number(discountRate),
+      category: "category",
+      reviewsCount: 100,
+      stars: 4.5,
+      isOutOfStock: outOfStock,
+      description,
+      lowestPrice: Number(currentPrice) || Number(originalPrice),
+      highestPrice: Number(originalPrice) || Number(currentPrice),
+      averagePrice: Number(currentPrice) || Number(originalPrice),
+    };
+
+    return data;
   } catch (error: any) {
     throw new Error(`Failed to scrape product: ${error.message}`);
   }
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,7 +1,83 @@
+import { PriceHistoryItem } from "@/types";
+
 export function extractPrice({ elements }: { elements: any }) {
   for (const element of elements) {
     const priceText = element.text().trim();
-    if (priceText) return priceText.replace(/\D/g, "");
+
+    if (priceText) {
+      const cleanPrice = priceText.replace(/[^\d.]/g, "");
+      let firstPrice;
+      if (cleanPrice) firstPrice = cleanPrice.match(/\d+\.\d{2}/)?.[0];
+      return firstPrice || cleanPrice;
+    }
   }
   return "";
 }
+
+export function extractCurrency({ element }: { element: any }) {
+  const currencyText = element.text().trim().slice(0, 1);
+  return currencyText ? currencyText : "";
+}
+
+export function extractDescription({ cheerio$: $ }: { cheerio$: any }) {
+  const selectors = [".a-unordered-list .a-list-item", ".a-expander-content p"];
+
+  for (const selector of selectors) {
+    const elements = $(selector);
+    if (elements.length > 0) {
+      const textContent = elements
+        .map((_: any, element: any) => $(element).text().trim())
+        .get()
+        .join("\n");
+      return textContent;
+    }
+  }
+
+  return "";
+}
+
+export function getHighestPrice({
+  priceList,
+}: {
+  priceList: PriceHistoryItem[];
+}) {
+  let highestPrice = priceList[0];
+  for (let i = 0; i < priceList.length; i++) {
+    if (priceList[i].price > highestPrice.price) {
+      highestPrice = priceList[i];
+    }
+  }
+
+  return highestPrice.price;
+}
+
+export function getLowestPrice({
+  priceList,
+}: {
+  priceList: PriceHistoryItem[];
+}) {
+  let lowestPrice = priceList[0];
+
+  for (let i = 0; i < priceList.length; i++) {
+    if (priceList[i].price < lowestPrice.price) lowestPrice = priceList[i];
+  }
+
+  return lowestPrice.price;
+}
+
+export function getAveragePrice({
+  priceList,
+}: {
+  priceList: PriceHistoryItem[];
+}) {
+  const sumOfPrices = priceList.reduce((acc, curr) => acc + curr.price, 0);
+  const averagePrice = sumOfPrices / priceList.length || 0;
+  return averagePrice;
+}
+
+export const formatNumber = ({ num = 0 }: { num: number }) => {
+  return num.toLocaleString(undefined, {
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  });
+};
