83cfc8acc4eca47958eb793731cc1afcf92e3431
samaksh mittal <sam.mittal44@gmail.com>
2024-08-07 23:12:17 +0530

    chore: add productSchema

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -16,6 +16,9 @@ export async function scrapeAndStoreProduct({
     connectToDB();
     const scrapedProduct = await scrapeAmazonProduct({ url: productUrl });
     if (!scrapedProduct) return;
+
+    let product = scrapedProduct;
+    // const existingProduct=await
   } catch (error: any) {
     throw new Error(`Failed to create/update product ${error.message}`);
   }
