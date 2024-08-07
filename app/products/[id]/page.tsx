942964e3970e5012f66678582e3933f7f2ef7580
samaksh mittal <sam.mittal44@gmail.com>
2024-08-08 03:56:41 +0530

    feat: add ui for product page and priceinfocard

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,7 +1,202 @@
-import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { redirect } from "next/navigation";
 
-const ProductDetails = () => {
-  return <div>ProductDetails</div>;
+import { Product } from "@/types";
+import { formatNumber } from "@/lib/utils";
+import { getProductById } from "@/lib/actions";
+
+import PriceInfoCard from "@/components/PriceInfoCard";
+
+type Props = {
+  params: { id: string };
+};
+
+const ProductDetails = async ({ params: { id } }: Props) => {
+  const product: Product = await getProductById({ productId: id });
+  if (!product) redirect("/");
+  const {
+    url,
+    title,
+    image,
+    stars,
+    currency,
+    reviewsCount,
+    currentPrice,
+    averagePrice,
+    highestPrice,
+    lowestPrice,
+    originalPrice,
+  } = product;
+
+  return (
+    <div className="product-container">
+      <div className="flex gap-28 xl:flex-row flex-col">
+        <div className="product-image">
+          <Image
+            src={image}
+            alt={title}
+            width={580}
+            height={400}
+            className="mx-auto"
+          />
+        </div>
+
+        <div className="flex-1 flex flex-col">
+          <div className="flex justify-between items-start gap-5 flex-wrap pb-6">
+            <div className="flex flex-col gap-3">
+              <p className="text-[28px] text-secondary font-semibold">
+                {title}
+              </p>
+
+              <Link
+                href={url}
+                target="_blank"
+                className="text-base text-black opacity-50"
+              >
+                Visit Product
+              </Link>
+            </div>
+
+            <div className="flex items-center gap-3">
+              <div className="product-hearts">
+                <Image
+                  src="/assets/icons/red-heart.svg"
+                  alt="heart"
+                  width={20}
+                  height={20}
+                />
+
+                <p className="text-base font-semibold text-[#D46F77]">
+                  {reviewsCount}
+                </p>
+              </div>
+
+              <div className="p-2 bg-white-200 rounded-10">
+                <Image
+                  src="/assets/icons/bookmark.svg"
+                  alt="bookmark"
+                  width={20}
+                  height={20}
+                />
+              </div>
+
+              <div className="p-2 bg-white-200 rounded-10">
+                <Image
+                  src="/assets/icons/share.svg"
+                  alt="share"
+                  width={20}
+                  height={20}
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className="product-info">
+            <div className="flex flex-col gap-2">
+              <p className="text-[34px] text-secondary font-bold">
+                {currency} {formatNumber({ num: currentPrice })}
+              </p>
+              <p className="text-[21px] text-black opacity-50 line-through">
+                {currency} {formatNumber({ num: originalPrice })}
+              </p>
+            </div>
+
+            <div className="flex flex-col gap-4">
+              <div className="flex gap-3">
+                <div className="product-stars">
+                  <Image
+                    src="/assets/icons/star.svg"
+                    alt="star"
+                    width={16}
+                    height={16}
+                  />
+                  <p className="text-sm text-primary-orange font-semibold">
+                    {stars || "25"}
+                  </p>
+                </div>
+
+                <div className="product-reviews">
+                  <Image
+                    src="/assets/icons/comment.svg"
+                    alt="comment"
+                    width={16}
+                    height={16}
+                  />
+                  <p className="text-sm text-secondary font-semibold">
+                    {reviewsCount} Reviews
+                  </p>
+                </div>
+              </div>
+
+              <p className="text-sm text-black opacity-50">
+                <span className="text-primary-green font-semibold">93% </span>{" "}
+                of buyers have recommeded this.
+              </p>
+            </div>
+          </div>
+
+          <div className="my-7 flex flex-col gap-5">
+            <div className="flex gap-5 flex-wrap">
+              <PriceInfoCard
+                title="Current Price"
+                iconSrc="/assets/icons/price-tag.svg"
+                value={`${currency} ${formatNumber({
+                  num: currentPrice,
+                })}`}
+              />
+              <PriceInfoCard
+                title="Average Price"
+                iconSrc="/assets/icons/chart.svg"
+                value={`${currency} ${formatNumber({
+                  num: averagePrice,
+                })}`}
+              />
+              <PriceInfoCard
+                title="Highest Price"
+                iconSrc="/assets/icons/arrow-up.svg"
+                value={`${currency} ${formatNumber({
+                  num: highestPrice,
+                })}`}
+              />
+              <PriceInfoCard
+                title="Lowest Price"
+                iconSrc="/assets/icons/arrow-down.svg"
+                value={`${currency} ${formatNumber({
+                  num: lowestPrice,
+                })}`}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-16">
+        <div className="flex flex-col gap-5">
+          <h3 className="text-2xl text-secondary font-semibold">
+            Product Description
+          </h3>
+
+          <div className="flex flex-col gap-4">
+            {product?.description?.split("\n")}
+          </div>
+        </div>
+
+        <button className="btn w-fit mx-auto flex items-center justify-center gap-3 min-w-[200px]">
+          <Image
+            src="/assets/icons/bag.svg"
+            alt="check"
+            width={22}
+            height={22}
+          />
+
+          <Link href="/" className="text-base text-white">
+            Buy Now
+          </Link>
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default ProductDetails;
---
+++ components/PriceInfoCard/index.tsx
@@ -0,0 +1,2 @@
+import comp from "./PriceInfoCard";
+export default comp;
