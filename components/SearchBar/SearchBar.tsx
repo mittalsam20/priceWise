1e598f1b2a7f24be369ab1482c205303a76f1bd2
samaksh mittal <sam.mittal44@gmail.com>
2024-08-07 14:09:33 +0530

    chore: add basic ui for searchbar

--- components/SearchBar/SearchBar.tsx
+++ components/SearchBar/SearchBar.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+"use client";
 
 const SearchBar = () => {
-  return <div>SearchBar</div>;
+  const handleSubmit = () => {};
+
+  return (
+    <form onSubmit={handleSubmit} className="flex flex-wrap gap-4 mt-12 ">
+      <input
+        type="text"
+        className="searchbar-input"
+        placeholder={"Enter Product Link"}
+      />
+
+      <button type={"submit"} className="searchbar-btn">
+        {"Search"}
+      </button>
+    </form>
+  );
 };
 
 export default SearchBar;
